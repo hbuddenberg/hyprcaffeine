be0f1f35e2b603d20c47c6be88e4cbde0cccbea7
HyprCaffeine <hyprcaffeine@local>
2026-05-15 21:58:49 -0400

    fix: use U+F0DC0 (nf-md-infinity) for infinity icon - MD icons work in Walker

--- scripts/icons.sh
+++ scripts/icons.sh
@@ -2,4 +2,4 @@
 # Nerd Font icons for HyprCaffeine
 readonly NF_COFFEE=""  # nf-md-coffee
 readonly NF_COFFEE_OUTLINE=""  # nf-md-coffee_outline
-readonly NF_INFINITE="∞"  # nf-fa-infinity
+readonly NF_INFINITE="󰷀"  # nf-fa-infinity
